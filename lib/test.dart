7a614b63b2b7ddafcec1caf7f8359f85914d3588
banksiwat641 <banksiwat.sr@gmail.com>
2021-12-16 14:42:33 +0700

    s

--- lib/test.dart
+++ lib/test.dart
@@ -4,17 +4,43 @@ import 'dart:math';
 void main() {
   var i = 0;
   var r = new Random();
-  var answer = r.nextInt(10);
-  //print(answer);
+  var answer = r.nextInt(100);
+  var guess;
+  var count=0;
+  print(answer);
+  print("╔═════════════════════════════════════════");
+  print("║            GUESS THE NUMBER");
+  print("╟─────────────────────────────────────────");
   do {
-    stdout.write('Please enter your number: ');
+
+    stdout.write("║ "+'Guess the number between 1 and 100: ');
     var input = stdin.readLineSync();
-    var guess = int.tryParse(input!);
-    if (guess == answer) {
-      print("CORRECT");
-      break;
-    } else {
-      print("Please try again");
+    guess = int.tryParse(input!);
+    if(guess==null) {
+      print("ข้อมูลผิดพลาด");
+    }else {
+      if (guess != answer) {
+        if (guess < answer) {
+
+          print("║➜ "+input + " TOO LOW Please try again");
+          print("╟─────────────────────────────────────────");
+        }
+        if (guess > answer) {
+
+          print("║➜ "+input + " TOO HIGH! Please try again");
+          print("╟─────────────────────────────────────────");
+        }
+
+        count++;
+      } else {
+        count++;
+        print("║➜ "+"❤CORRECT!❤ total guesses: " + count.toString() +"❤");
+        print("╟─────────────────────────────────────────");
+        print("║              THE END");
+        print("╚═════════════════════════════════════════");
+
+        break;
+      }
     }
   }while(true);
 
